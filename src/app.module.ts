e169b59649aa687439a20ff23fc08f4433d9d2f8
yogiex <priyagung.yoggi@gmail.com>
2023-10-01 01:02:41 +0700

    first update module user and assets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import { AssetsModule } from './assets/assets.module';
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+    }),
+    UsersModule,
+    AssetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
